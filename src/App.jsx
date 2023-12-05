30ba4e7e64dd10efe98a5d9328ccb040f30b1c09
mhsenam <mohsenamini1081@gmail.com>
2023-12-05 17:41:52 +0330

    added Contact Page with animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import { Home, About, Projects, Contacts } from "./pages";
+import { Home, About, Projects, Contact } from "./pages";
 import Navbar from "./components/Navbar";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,151 @@
+import { useState, useRef, Suspense } from "react";
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import Fox from "../models/Fox";
+import Loader from "../components/Loader";
+const Contact = () => {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [isLoading, setIsLoading] = useState(false);
+  const formRef = useRef(null);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: [e.target.value] });
+  };
+  const handleFocus = () => {
+    setCurrentAnimation("walk");
+  };
+  const handleBlur = () => {
+    setCurrentAnimation("idle");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation("hit");
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Mohsen",
+          from_email: form.email,
+          to_email: "mohsenamini1081@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        // show success message
+        // hide an alert
+        setForm({ name: "", email: "", message: "" });
+
+        setTimeout(() => {
+          setCurrentAnimation("idle");
+        }, [3000]);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setCurrentAnimation("idle");
+        // show error message
+      });
+  };
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <div className="flex justify-between flex-row items-center lg:h-[100px]">
+          <h1 className="w-full head-text texc">Get in touch</h1>
+          <div className="w-1/2">
+            <Canvas
+              camera={{
+                position: [0, 0, 5],
+                fov: 75,
+                near: 0.1,
+                far: 1000,
+              }}
+            >
+              <directionalLight position={[0, 0, 1]} intensity={2.5} />
+              <ambientLight intensity={1} />
+              <pointLight position={[5, 10, 0]} intensity={2} />
+              <spotLight
+                position={[10, 10, 10]}
+                angle={0.15}
+                penumbra={1}
+                intensity={2}
+              />
+
+              <Suspense fallback={<Loader />}>
+                <Fox
+                  currentAnimation={currentAnimation}
+                  position={[0.5, 0.35, 0]}
+                  rotation={[12.629, -0.6, 0]}
+                  scale={[0.5, 0.5, 0.5]}
+                />
+              </Suspense>
+            </Canvas>
+          </div>
+        </div>
+
+        <form className="w-full flex flex-col gap-7" onSubmit={handleSubmit}>
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Mohsen"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="example@example.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              className="textarea"
+              rows={4}
+              placeholder="Let me know how I can help you!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            disabled={isLoading}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
